11ed063c21006bea3205b48da8d88e04a241e38b
krutik39 <krutik391@gmail.com>
2017-03-16 18:22:51 -0700

    Color Range

--- script1.py
+++ script1.py
@@ -7,14 +7,8 @@ df = pandas.read_csv("Volcanoes-USA.txt")
 map=folium.Map(location=[45.372, -121.697],zoom_start=4, tiles='Stamen Terrain')
 
 
-for lat, lon, name in zip(df['LAT'], df['LON'], df['NAME']):
-    folium.Marker([lat,lon], popup=name, icon=folium.Icon(color='red')).add_to(map)
+for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    folium.Marker([lat,lon], popup=name, icon=folium.Icon(color='green' if elev in range(0,1000)else 'orange' if elev in range(1000,3000) else 'red')).add_to(map)
 
 #map.add_child(folium.Marker(location=[45.3288, -121.6625], popup='Mt. Hood Meadows', icon=folium.Icon(color='white',icon_color='red')) )
-
-# folium.Marker([45.3311, -121.7113],
-# popup='Timberline Lodge',
-# icon=folium.Icon(color='green')
-# ).add_to(map)
-
 map.save('test.html')
