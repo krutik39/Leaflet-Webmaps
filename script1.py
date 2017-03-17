962032cefdf030748afcb155ac0791030ea57891
krutik39 <krutik391@gmail.com>
2017-03-16 18:35:53 -0700

    More Organized

--- script1.py
+++ script1.py
@@ -6,9 +6,19 @@ df = pandas.read_csv("Volcanoes-USA.txt")
 
 map=folium.Map(location=[45.372, -121.697],zoom_start=4, tiles='Stamen Terrain')
 
+def color(elev):
+    if elev in range(0,1000):
+        col='green'
+    elif elev in range(1000,3000):
+        col='orange'
+    else:
+        col='red'
+    return col
+
+
 
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    folium.Marker([lat,lon], popup=name, icon=folium.Icon(color='green' if elev in range(0,1000)else 'orange' if elev in range(1000,3000) else 'red')).add_to(map)
+    folium.Marker([lat,lon], popup=name, icon=folium.Icon(color=color(elev))).add_to(map)
 
 #map.add_child(folium.Marker(location=[45.3288, -121.6625], popup='Mt. Hood Meadows', icon=folium.Icon(color='white',icon_color='red')) )
 map.save('test.html')
