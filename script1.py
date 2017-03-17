8ae568cc6569fa7ef2e9b8feff929d18944357ae
krutik39 <krutik391@gmail.com>
2017-03-16 18:06:07 -0700

    Markers from CSV data

--- script1.py
+++ script1.py
@@ -1,7 +1,20 @@
 import folium
-map=folium.Map(location=[45.372, -121.697],zoom_start=12, tiles='Stamen Terrain')
+import pandas
 
-map.simple_marker(location=[45.3288, -121.6625], popup='Mt Hood Meadows', marker_color='red')
-map.simple_marker(location=[45.3311, -121.7311], popup='Timberlake Lodge', marker_color='green')
+df = pandas.read_csv("Volcanoes-USA.txt")
+
+
+map=folium.Map(location=[45.372, -121.697],zoom_start=4, tiles='Stamen Terrain')
+
+
+for lat, lon, name in zip(df['LAT'], df['LON'], df['NAME']):
+    folium.Marker([lat,lon], popup=name, icon=folium.Icon(color='red')).add_to(map)
+
 #map.add_child(folium.Marker(location=[45.3288, -121.6625], popup='Mt. Hood Meadows', icon=folium.Icon(color='white',icon_color='red')) )
+
+# folium.Marker([45.3311, -121.7113],
+# popup='Timberline Lodge',
+# icon=folium.Icon(color='green')
+# ).add_to(map)
+
 map.save('test.html')
